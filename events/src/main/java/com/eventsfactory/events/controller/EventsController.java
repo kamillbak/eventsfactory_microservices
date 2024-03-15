f7ff30682c27f6f25b492a006535a9e3685a6f55
kamillbak <kamil1427@o2.pl>
2024-03-15 20:42:14 +0100

    better responses

--- events/src/main/java/com/eventsfactory/events/controller/EventsController.java
+++ events/src/main/java/com/eventsfactory/events/controller/EventsController.java
@@ -1,9 +1,13 @@
 package com.eventsfactory.events.controller;
 
+import com.eventsfactory.events.constants.EventsConstants;
 import com.eventsfactory.events.dto.EventsDto;
+import com.eventsfactory.events.dto.ResponseDto;
 import com.eventsfactory.events.services.api.EventsService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +19,47 @@ public class EventsController {
     private EventsService eventsService;
 
     @PostMapping("/create")
-    public void createEvent(@RequestBody EventsDto eventsDto) {
+    public ResponseEntity<ResponseDto> createEvent(@RequestBody EventsDto eventsDto) {
         eventsService.createEvent(eventsDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(EventsConstants.STATUS_201,EventsConstants.MESSAGE_201));
     }
 
     @GetMapping("/get")
-    public void getEvent(@RequestParam Long eventId) {
-        eventsService.getEvent(eventId);
+    public ResponseEntity<EventsDto> getEvent(@RequestParam Long eventId) {
+       EventsDto eventsDto = eventsService.getEvent(eventId);
+       return ResponseEntity.status(HttpStatus.OK).body(eventsDto);
     }
 
     @PutMapping("/update")
-    public void updateEvent(@RequestParam Long eventId, @RequestBody EventsDto eventsDto) {
-        eventsService.updateEvent(eventId, eventsDto);
+    public ResponseEntity<ResponseDto> updateEvent(@RequestParam Long eventId, @RequestBody EventsDto eventsDto) {
+        boolean isUpdated = eventsService.updateEvent(eventId, eventsDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(EventsConstants.STATUS_200, EventsConstants.MESSAGE_200));
+        }
+        else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(EventsConstants.STATUS_417, EventsConstants.MESSAGE_417_UPDATE));
+        }
     }
 
-    @DeleteMapping("/delete/")
-    public void deleteEvent(@RequestParam Long eventId) {
-        eventsService.deleteEvent(eventId);
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteEvent(@RequestParam Long eventId) {
+        boolean isDeleted = eventsService.deleteEvent(eventId);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(EventsConstants.STATUS_200, EventsConstants.MESSAGE_200));
+        }
+        else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(EventsConstants.STATUS_417, EventsConstants.MESSAGE_417_DELETE));
+        }
     }
 
 }
---
+++ events/src/main/java/com/eventsfactory/events/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.eventsfactory.events.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+    private String statusMsg;
+}
