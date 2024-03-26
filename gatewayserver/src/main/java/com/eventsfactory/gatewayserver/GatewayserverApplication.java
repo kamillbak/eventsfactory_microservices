074d8ba67c98c411f8e5e1234ddb5c1023882f9e
kamillbak <kamil1427@o2.pl>
2024-03-26 20:25:49 +0100

    gateway server configuration

--- gatewayserver/src/main/java/com/eventsfactory/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eventsfactory/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,11 @@
 package com.eventsfactory.gatewayserver;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +14,8 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public HttpClient httpClient() {
+		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
 }
