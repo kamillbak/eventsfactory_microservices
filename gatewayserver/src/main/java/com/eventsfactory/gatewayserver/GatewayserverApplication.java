f9c816eef1a2491a5eddb1bf787da19d7abfb915
kamillbak <kamil1427@o2.pl>
2024-03-27 11:31:54 +0100

    custom routes

--- gatewayserver/src/main/java/com/eventsfactory/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eventsfactory/gatewayserver/GatewayserverApplication.java
@@ -3,6 +3,8 @@ package com.eventsfactory.gatewayserver;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import reactor.netty.http.client.HttpClient;
 
@@ -18,4 +20,23 @@ public class GatewayserverApplication {
 	public HttpClient httpClient() {
 		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
 	}
+
+	@Bean
+	public RouteLocator eventsFactoryRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/events/**")
+						.filters(f -> f.rewritePath("/events/(?<segment>.*)", "/${segment}"))
+						.uri("lb://EVENTS"))
+				.route(p -> p
+						.path("/locations/**")
+						.filters(f -> f.rewritePath("/locations/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOCATIONS"))
+				.route(p -> p
+						.path("/custom/users/**")
+						.filters(f -> f.rewritePath("/custom/users/(?<segment>.*)", "/${segment}"))
+						.uri("lb://USERS"))
+				.build();
+	}
+
 }
